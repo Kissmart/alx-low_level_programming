9d2874104d27ec4538992c0aef7b09d684d05038
Kissmart <kissmartmoeketsi@gmail.com>
2023-03-19 17:10:17 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Project
+0x02-functions_nested_loops
---
+++ 0x02-functions_nested_loops/headers.h
@@ -0,0 +1,10 @@
+#define HEADERS_H_
+
+/*
+ * File: headers.h
+ * Auth: Franklin Obasi
+ * Desc: Header file containing declaration for all functions used
+ * 	 in the 0x02-functions_nested_loops directory
+ */
+
+int _putchar(char);
