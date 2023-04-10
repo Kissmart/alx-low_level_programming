2be85a1e94d7123617a53dfb0eebf6a9ecc31ac7
Kissmart <kissmartmoeketsi@gmail.com>
2023-04-10 10:32:08 -0700

    argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -29,12 +29,14 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-for (j = 0; j < 5 && num >= 0; j++)
+
+	for (j = 0; j < 5 && num >= 0; j++)
 	{
 		while (num >= coins[j])
 		{
 			result++;
-			num -= coins[j];}
+			num -= coins[j];
+		}
 	}
 
 	printf("%d\n", result);
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -35,7 +35,8 @@ int _atoi(char *s)
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			f = 0;}
+			f = 0;
+		}
 		i++;
 	}
 
@@ -70,4 +71,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
